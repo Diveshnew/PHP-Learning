6a7b177e0616262db4710641ae2b7959558514af
Divesh <ds7699864@gmail.com>
2023-09-24 22:53:06 +0530

    little practice

---
+++ 1.4_DataTypes_TypeCasting.php
@@ -0,0 +1,30 @@
+<?php
+
+// Data Types and type Casting
+
+
+# 4. Scalar Types
+    #Bool - true/false
+    $completed = true;
+    #int - 1,2,3,4 (no decimal)
+    $score = 75;
+    #float - 1.5 , 0.1 , 0.005, -15.6
+    $price = 0.99;
+    #string - "Gio", "Hello World"
+    $Greeting = 'Hello Divi';
+
+    echo $completed;
+    echo $score;
+    echo $price;
+    echo $Greeting;
+
+#4. Compound Types
+    #array
+    #object
+    #callable
+    #iterable
+
+#2. Special Types
+    #resource
+    #null
+
